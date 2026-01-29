63721f005809f086834ee55e4784c52cf7b4c53b
stackotter <stackotter@stackotter.dev>
2026-01-30 00:34:31 +1000

    Add Mia's FOSDEM talk link page

---
+++ src/routes/mia-fosdem-talk/+page.svelte
@@ -0,0 +1,31 @@
+<style>
+  a {
+    color: lightblue;
+    text-decoration: underline;
+  }
+
+  #content {
+    width: 80ch;
+    margin: auto;
+    margin-top: 4rem;
+  }
+
+  h1 {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 1rem;
+  }
+
+  ul {
+    list-style: inside;
+  }
+</style>
+
+<div id="content">
+  <h1>Music Player builds (<a href="https://github.com/moreSwift/swift-cross-ui/tree/stackotter/music_player/Examples/Sources/MusicPlayerExample">src</a>)</h1>
+  <ul>
+    <li><a href="https://drive.google.com/file/d/1rDYBqiZ1v5qV6_058HU4Ca79y6cGuQYt/view?usp=sharing">macOS x86_64</a></li>
+    <li><a href="https://drive.google.com/file/d/1tMKLbCS8xTd1Qka-0qZIWfDv4qjEjNP1/view?usp=sharing">macOS arm64</a></li>
+    <li><a href="https://drive.google.com/file/d/1vDoPGzSF6evxEMLaLnoIuXJVXqlZFNZU/view?usp=sharing">Linux x86_64</a></li>
+  </ul>
+</div>
