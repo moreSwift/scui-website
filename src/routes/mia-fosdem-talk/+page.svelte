ca85c110ee88b5aca3217ba52e2ef4f5eefd3c58
stackotter <stackotter@stackotter.dev>
2026-01-30 01:13:01 +1000

    Add Discord link, Windows build, and sponsor links to FOSDEM talk page

--- src/routes/mia-fosdem-talk/+page.svelte
+++ src/routes/mia-fosdem-talk/+page.svelte
@@ -13,7 +13,8 @@
   h1 {
     font-size: 2rem;
     font-weight: bold;
-    margin-bottom: 1rem;
+    margin-bottom: 0.5rem;
+    margin-top: 1rem;
   }
 
   ul {
@@ -22,10 +23,23 @@
 </style>
 
 <div id="content">
+  <h1>Community</h1>
+  <p>Join the <a href="https://discord.gg/fw2trT48ny">moreSwift Discord server</a>!</p>
   <h1>Music Player builds (<a href="https://github.com/moreSwift/swift-cross-ui/tree/stackotter/music_player/Examples/Sources/MusicPlayerExample">src</a>)</h1>
   <ul>
     <li><a href="https://drive.google.com/file/d/1rDYBqiZ1v5qV6_058HU4Ca79y6cGuQYt/view?usp=sharing">macOS x86_64</a></li>
     <li><a href="https://drive.google.com/file/d/1tMKLbCS8xTd1Qka-0qZIWfDv4qjEjNP1/view?usp=sharing">macOS arm64</a></li>
     <li><a href="https://drive.google.com/file/d/1vDoPGzSF6evxEMLaLnoIuXJVXqlZFNZU/view?usp=sharing">Linux x86_64</a></li>
+    <li><a href="https://drive.google.com/file/d/14nXkmz7SaAUjdMmpB_PeMhYTeyx37e8t/view?usp=sharing">Windows x86_64</a></li>
   </ul>
+  <p style="margin-top: 0.5rem">
+    Note: The Windows version of the music player requires that you install <a href="https://aka.ms/windowsappsdk/1.5/1.5.240205001-preview1/windowsappruntimeinstall-x64.exe">version 1.5.240205001-preview1 WindowsAppSDK runtime</a>. This is a limitation of Swift Bundler + SwiftCrossUI that we plan to address in the near future (once Android support is off the ground).
+  </p>
+  <h1>Interested in sponsoring?</h1>
+  <p>
+      moreSwift is a relatively new organisation and we're still figuring out how best to accept donations. If you or your company are interested in becoming a sponsor of moreSwift, please contact stackotter at <a href="mailto:stackotter@stackotter.dev">stackotter@stackotter.dev</a> or via any of his other <a href="https://stackotter.dev/contact">contact details</a>.
+  </p>
+  <p style="margin-top: 0.5rem">
+    For quick donations you can sponsor stackotter directly via his <a href="https://github.com/sponsors/stackotter">GitHub Sponsors page</a>.
+  </p>
 </div>
